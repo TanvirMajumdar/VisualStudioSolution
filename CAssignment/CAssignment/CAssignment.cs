7a280aac16bda82f09c4f3631779a08bb1f610d4
Tanvir Majumdar <tanvir.majumdar.03@gmail.com>
2021-11-17 20:57:09 -0600

    Initial commit

---
+++ CAssignment/CAssignment/CAssignment.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAssignment
+{
+    class CAssignment
+    {
+
+        public void DisplayArray(int[] inputArray)
+        {
+
+            int inputArrayLength = inputArray.Length;
+            int[] outputArray= new int[inputArrayLength * 2];
+
+            for(int i=0; i< inputArrayLength*2; i++)
+            {
+                if (i < inputArrayLength)
+                {
+                    outputArray[i] = inputArray[i];
+                }
+                else
+                {
+                    outputArray[i] = inputArray[i - inputArrayLength];
+                }
+            }
+
+            Console.WriteLine("Input numbers");
+
+            for (int i = 0; i < inputArrayLength; i++)
+            {
+                Console.Write(inputArray[i] + "\t");
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Output numbers");
+
+            for (int i = 0; i < inputArrayLength*2; i++)
+            {
+                Console.Write(outputArray[i]+"\t");
+            }
+
+
+        }
+
+    }
+}
---
+++ CAssignment/CAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CAssignment cAssignment = new CAssignment();
+
+            int inputArrayLength;
+
+            Console.WriteLine("Enter length of the input Array");
+            inputArrayLength = Int32.Parse(Console.ReadLine());
+
+            if (inputArrayLength <= 0)
+            {
+                Console.WriteLine("Array length is invalid. Program is exiting...");
+                System.Environment.Exit(0);
+            }
+            else 
+            { 
+            int[] inputArray = new int[inputArrayLength];
+
+            Console.WriteLine("Now Give " + inputArrayLength + " numbers for input array: ");
+
+            for (int i = 0; i < inputArrayLength; i++)
+            {
+                inputArray[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            cAssignment.DisplayArray(inputArray);
+            }
+
+        }
+    }
+}
